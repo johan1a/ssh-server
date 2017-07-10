72e65b371bd19b7904268ebd6575d42d2b0d07b9
Johan Andersson <joha@netcompany.com>
2017-07-10 19:38:28 +0200

    add ssh server template

---
+++ .profile.d/ssh_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo $0: creating public and private key files
+
+# Create the .ssh directory
+mkdir -p ${HOME}/.ssh
+chmod 700 ${HOME}/.ssh
+
+# Create the public and private key files from the environment variables.
+echo "${SSH_PUBLIC_KEY}" > ${HOME}/.ssh/heroku_ssh_rsa.pub
+chmod 644 ${HOME}/.ssh/heroku_ssh_rsa.pub
+
+# Note use of double quotes, required to preserve newlines
+echo "${SSH_PRIVATE_KEY}" > ${HOME}/.ssh/heroku_ssh_rsa
+chmod 600 ${HOME}/.ssh/heroku_ssh_rsa
+
+echo "${CLIENT_PUBLIC_KEY}" >> ${HOME}/.ssh/ssh_user_ca
+chmod 644 ${HOME}/.ssh/ssh_user_ca
+
+# Preload the known_hosts file  (see "version 2" below)
+
+# Start the SSH tunnel if not already running
+#SSH_CMD="ssh -f -i ${HOME}/.ssh/heroku_ssh_rsa -N -L 3307:${REMOTE_MYSQL_HOST}:3306 ${REMOTE_USER}@${REMOTE_SITE}"
+
+#PID=`pgrep -f "${SSH_CMD}"`
+#if [ $PID ] ; then
+#    echo $0: tunnel already running on ${PID}
+#else
+#    echo $0 launching tunnel
+#    $SSH_CMD
+#fi
--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+import asyncio, asyncssh, crypt, sys, os
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+ssh_dir = os.environ.get('HOME') + "/.ssh"
 
+def handle_client(process):
+    process.stdout.write('Welcome to my SSH server, %s!\n' %
+                         process.channel.get_extra_info('username'))
+    process.exit(0)
+
+async def start_server():
+    await asyncssh.listen('', 8022, server_host_keys=[ssh_dir + '/heroku_ssh_rsa'],
+                          authorized_client_keys=ssh_dir + '/ssh_user_ca',
+                          process_factory=handle_client)
+
+loop = asyncio.get_event_loop()
+
+try:
+    loop.run_until_complete(start_server())
+except (OSError, asyncssh.Error) as exc:
+    sys.exit('Error starting server: ' + str(exc))
+
+loop.run_forever()
